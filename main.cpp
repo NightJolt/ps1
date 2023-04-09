22733596458a442a00e4370e9c359ca49ee785ee
NightJolt <mk.ff.dev@gmail.com>
2023-04-09 15:11:17 +0400

    added imgui

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 #include "core/hardreg.h"
 #include "core/nodevice.h"
 
+#include <glfw/glfw3.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+
 int main() {
     ps1::bus_t bus;
     ps1::cpu_t cpu(&bus);
@@ -17,10 +22,42 @@ int main() {
 
     ps1::nodevice_t nodevice;
     bus.add_device(&nodevice,   { 0xFFFE0130, 4 });
-    
-    while (true) {
-        cpu.tick();
+
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(640, 480, "ps1", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+    ImGui::StyleColorsDark();
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // cpu.tick();
+        
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Text("This is some useful text.");
+        ImGui::End();
+
+        ImGui::Render();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     
     return 0;
 }
