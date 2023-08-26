bfe9654731790f158f89d8e4356e2524ccd589b1
NightJolt <mk.ff.dev@gmail.com>
2023-08-26 22:22:58 +0400

    added gp0_set_draw_mode, gp1_reset

--- core/gpu.h
+++ core/gpu.h
@@ -20,8 +20,8 @@ namespace ps1 {
             };
 
             struct {
-                uint32_t texture_page_x_base_t : 4;
-                uint32_t texture_page_y_base_t : 1;
+                uint32_t texture_page_x_base : 4;
+                uint32_t texture_page_y_base : 1;
                 uint32_t semi_transparency : 2;
                 texture_depth_t texture_depth : 2;
                 uint32_t dither : 1;
@@ -62,13 +62,113 @@ namespace ps1 {
 
     struct gpu_t {
         gpu_stat_t stat;
+
+        bool rect_texture_x_flip;
+        bool rect_texture_y_flip;
+
+        uint8_t texture_window_x_mask;
+        uint8_t texture_window_y_mask;
+        uint8_t texture_window_x_offset;
+        uint8_t texture_window_y_offset;
+        uint16_t drawing_area_left;
+        uint16_t drawing_area_top;
+        uint16_t drawing_area_right;
+        uint16_t drawing_area_bottom;
+        int16_t drawing_offset_x;
+        int16_t drawing_offset_y;
+        uint16_t display_vram_x_start;
+        uint16_t display_vram_y_start;
+        uint16_t display_horiz_start;
+        uint16_t display_horiz_end;
+        uint16_t display_line_start;
+        uint16_t display_line_end;
     };
 
     void gpu_init(gpu_t*);
     void gpu_exit(gpu_t*);
 
+    inline void gp0_set_draw_mode(gpu_t* gpu, uint32_t value) {
+        gpu_stat_t stat;
+        stat.raw = value;
+
+        gpu->stat.texture_page_x_base = stat.texture_page_x_base;
+        gpu->stat.texture_page_y_base = stat.texture_page_y_base;
+        gpu->stat.semi_transparency = stat.semi_transparency;
+        gpu->stat.texture_depth = stat.texture_depth;
+        gpu->stat.dither = stat.dither;
+        gpu->stat.draw_to_display = stat.draw_to_display;
+        gpu->stat.texture_page_y_base_2 = stat.texture_page_y_base_2; // ? disable texture
+
+        gpu->rect_texture_x_flip = (value >> 12) & 0x1;
+        gpu->rect_texture_y_flip = (value >> 13) & 0x1;
+    }
+
+    inline void gp1_reset(gpu_t* gpu) {
+        gpu->stat.raw = 0x14802000;
+
+        gpu->rect_texture_x_flip = false;
+        gpu->rect_texture_y_flip = false;
+
+        gpu->texture_window_x_mask = 0;
+        gpu->texture_window_y_mask = 0;
+        gpu->texture_window_x_offset = 0;
+        gpu->texture_window_y_offset = 0;
+        gpu->drawing_area_left = 0;
+        gpu->drawing_area_top = 0;
+        gpu->drawing_area_right = 0;
+        gpu->drawing_area_bottom = 0;
+        gpu->drawing_offset_x = 0;
+        gpu->drawing_offset_y = 0;
+        gpu->display_vram_x_start = 0;
+        gpu->display_vram_y_start = 0;
+        gpu->display_horiz_start = 0x200;
+        gpu->display_horiz_end = 0xc00;
+        gpu->display_line_start = 0x10;
+        gpu->display_line_end = 0x100;
+
+        // todo: clear fifo and invalidate cache
+    }
+
     inline void gp0(gpu_t* gpu, uint32_t value) {
         uint32_t opcode = value >> 24;
+
+        switch(opcode) {
+            case 0xE1: {
+                gp0_set_draw_mode(gpu, value);
+
+                break;
+            }
+
+            case 0x00: {
+                // * NOP. does not take up space in fifo
+
+                break;
+            }
+
+            default: {
+                ASSERT(false, "ILLEGAL GP0 OPCODE");
+
+                break;
+            }
+        }
+    }
+
+    inline void gp1(gpu_t* gpu, uint32_t value) {
+        uint32_t opcode = value >> 24;
+
+        switch(opcode) {
+            case 0x00: {
+                gp1_reset(gpu);
+
+                break;
+            }
+
+            default: {
+                ASSERT(false, "ILLEGAL GP1 OPCODE");
+
+                break;
+            }
+        }
     }
 
     FETCH_FN(gpu_t) fetch(void* device, mem_addr_t offset) {
@@ -87,7 +187,7 @@ namespace ps1 {
         if (offset == 0) {
             gp0(gpu, value);
         } else if (offset == 4) {
-            gpu->stat.raw = value;
+            gp1(gpu, value);
         } else {
             ASSERT(false, "unhandled gpu store");
         }
